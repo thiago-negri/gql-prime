99ca69e06810123c99f47c4a8818c3894d7280f2
Thiago Negri <evohunz@gmail.com>
2024-03-08 20:33:18 -0300

    Add README

---
+++ README.md
@@ -0,0 +1,44 @@
+# GQL-Prime
+
+Main objective of this project is to create a foundation for GraphQL backend that has all required features for a production API and it is supposed to be
+easy to maintain. Includes:
+
+- Database access (Knex)
+- External cache read and write (Redis)
+- Dependency injection (Awilix)
+- GraphQL schema / resolvers (Mercurius)
+- HTTP server (Fastify)
+- Unit and integration tests
+
+It's still a work in progress.
+
+## Dependency Injection (Awilix)
+
+Classes from `./src/data` and `./src/services` are automatically made available in the Awilix container.
+Their default lifetime is singleton, meaning there's only one instance for the entire app.
+They are exposed according to the file name (e.g. `user-service.ts` becomes `userService` in the container).
+
+Functions from `./src/di-resolvers` are automatically made available as well, but their lifetime is scoped to
+a request.
+They are exposed according to the file name, with `di-resolver` removed from the suffix
+(e.g. `current-date-di-resolver.ts` becomes `currentDate` in the container).
+
+The name exposed
+
+The Awilix proxy object is not typed. To work around that, check type declaration at `./src/types/graphql-di-scope.ts`.
+
+Check `./src/config/config-awilix.ts` to see how it's setup.
+
+## GraphQL schema / resolvers (Mercurius)
+
+GraphQL schema is exposed through Mercurius. All files in `./src/gql-schema` are merged to form the final exposed schema.
+
+All files defined in `./src/gql-resolver` are merged and automatically added as resolvers to the GraphQL runtime.
+The exported objects are merged, so each gql-resolver file should export an object that declares a type in the exposed schema.
+
+For example, each query resolver needs to export `{ Query: ... }`. In the same way each mutation resolver needs to export `{ Mutation: ... }`.
+Type resolvers need export their type, e.g. `{ CustomType: ... }`.
+
+Multiple files can export the same type, the resolvers are combined/merged.
+
+Check `./src/config/config-mercurius.ts` to see how it's setup.
--- src/config/config-awilix.ts
+++ src/config/config-awilix.ts
@@ -1,10 +1,33 @@
+import path from 'path'
 import { fastifyAwilixPlugin } from '@fastify/awilix'
 import { Lifetime, asClass, asFunction, asValue } from 'awilix'
 import { type FastifyInstance } from 'fastify'
 
-import UsersData from '../data/users/users-data'
-import AuthService from '../services/auth-service'
-import userDiResolver from '../di-resolvers/user-di-resolver'
+/** user-service => userService */
+function formatDiClassName (fileName: string): string {
+  if (!fileName.endsWith('-data') && !fileName.endsWith('-service')) {
+    throw new Error(`Invalid service/data name: ${fileName}`)
+  }
+  const parts = fileName.split('-')
+  let result = parts[0]
+  for (let i = 1; i < parts.length; i++) {
+    result += parts[i].slice(0, 1).toUpperCase() + parts[i].slice(1)
+  }
+  return result
+}
+
+/** current-date-di-resolver => currentDate */
+function formatDiResolverName (fileName: string): string {
+  if (!fileName.endsWith('-di-resolver')) {
+    throw new Error(`Invalid DI resolver name: ${fileName}`)
+  }
+  const parts = fileName.split('-')
+  let result = parts[0]
+  for (let i = 1; i < parts.length - 2; i++) {
+    result += parts[i].slice(0, 1).toUpperCase() + parts[i].slice(1)
+  }
+  return result
+}
 
 async function configAwilix (app: FastifyInstance): Promise<void> {
   await app.register(fastifyAwilixPlugin, {
@@ -12,14 +35,33 @@ async function configAwilix (app: FastifyInstance): Promise<void> {
     disposeOnResponse: true,
     strictBooleanEnforced: true
   })
-  app.diContainer.register({
-    usersData: asClass(UsersData, { lifetime: Lifetime.SINGLETON }),
-    authService: asClass(AuthService, { lifetime: Lifetime.SINGLETON }),
-    user: asFunction(userDiResolver, { lifetime: Lifetime.SCOPED })
+  // Services and Data are SINGLETON by default
+  app.diContainer.loadModules([
+    path.join(__dirname, '../data/**/*.ts'),
+    path.join(__dirname, '../services/**/*.ts')
+  ], {
+    formatName: formatDiClassName,
+    resolverOptions: {
+      register: asClass,
+      lifetime: Lifetime.SINGLETON
+    }
   })
+  // DI-Resolvers are SCOPED by default
+  app.diContainer.loadModules([
+    path.join(__dirname, '../di-resolvers/**/*.ts')
+  ], {
+    formatName: formatDiResolverName,
+    resolverOptions: {
+      register: asFunction,
+      lifetime: Lifetime.SCOPED
+    }
+  })
+  // Add 'request' and 'reply' to DI scope
   app.addHook('onRequest', (request, reply, done) => {
-    request.diScope.register({ request: asValue(request) })
-    request.diScope.register({ reply: asValue(reply) })
+    request.diScope.register({
+      request: asValue(request),
+      reply: asValue(reply)
+    })
     done()
   })
 }
